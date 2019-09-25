494d9e1a8e9cb67d45625427b06a8d0e7882fc58
jmilis2000 <jahlil.milis97@gmail.com>
2019-09-25 17:29:13 -0400

    Implemented basic Firebase login and sign up 	modified:   Ensemble/Ensemble.Android/Ensemble.Android.csproj 	modified:   Ensemble/Ensemble.Android/MainActivity.cs 	modified:   Ensemble/Ensemble.Android/Resources/Resource.designer.cs 	new file:   Ensemble/Ensemble.Android/Resources/layout/Main.xml 	new file:   Ensemble/Ensemble.Android/Resources/layout/SignUp.xml 	new file:   Ensemble/Ensemble.Android/SignUp.cs 	modified:   Ensemble/Ensemble/Ensemble.csproj 	new file:   Ensemble/Resources/Resource.designer.cs

--- Ensemble/Ensemble.Android/MainActivity.cs
+++ Ensemble/Ensemble.Android/MainActivity.cs
@@ -6,30 +6,102 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Firebase;
+using Firebase.Auth;
+using static Android.Views.View;
+using Android.Gms.Tasks;
 using Android.Support.Design.Widget;
+
 
 namespace Ensemble.Droid
 {
     [Activity(Label = "Ensemble", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : Activity, IOnClickListener,IOnCompleteListener
     {
-        /**
-         * Method that runs when the main activity is created.
-         */
+        Button btnLogin;
+        EditText input_email, input_pwd;
+        TextView btnSignUp, btnForgetPwd;
+        RelativeLayout activity_main;
+        public static FirebaseApp app;
+        FirebaseAuth auth;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+            InitFirebaseAuth();
+
+            btnLogin = FindViewById<Button>(Resource.Id.login_btn_login);
+            input_email = FindViewById<EditText>(Resource.Id.login_email);
+            input_pwd = FindViewById<EditText>(Resource.Id.login_password);
+            btnSignUp = FindViewById<TextView>(Resource.Id.login_btn_sign_up);
+            btnForgetPwd = FindViewById<TextView>(Resource.Id.login_btn_forget_password);
+            activity_main = FindViewById<RelativeLayout>(Resource.Id.activity_main);
+
+            btnSignUp.SetOnClickListener(this);
+            btnLogin.SetOnClickListener(this);
+            btnForgetPwd.SetOnClickListener(this);
+            /*
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+            LoadApplication(new App());*/
+        }
+
+        private void InitFirebaseAuth()
+        {
+            var options = new FirebaseOptions.Builder()
+                .SetApplicationId("1:800755725006:android:964d63a2924e415158969f")
+                .SetApiKey("AIzaSyAJBWfWnAu-3pnw6VzHoQu17bZKghnZJOA")
+                .Build();
+            if (app == null)
+                app = FirebaseApp.InitializeApp(this, options);
+            auth = FirebaseAuth.GetInstance(app);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void OnClick(View v)
+        {
+            if (v.Id == Resource.Id.login_btn_sign_up)
+            {
+                StartActivity(new Android.Content.Intent(this, typeof(ForgetPassword)));
+                Finish();
+            }
+            else if (v.Id == Resource.Id.login_btn_sign_up)
+            {
+                StartActivity(new Android.Content.Intent(this, typeof(SignUp)));
+                Finish();
+            }
+            else if (v.Id == Resource.Id.login_btn_login)
+            {
+                LoginUser(input_email.Text, input_pwd.Text);
+            }
+        }
+
+        private void LoginUser(string email, string password)
+        {
+            auth.SignInWithEmailAndPassword(email, password).AddOnCompleteListener(this);
+        }
+
+        public void OnComplete(Task task)
+        {
+            if (task.IsSuccessful)
+            {
+                StartActivity(new Android.Content.Intent(this, typeof(MainActivity)));
+                Finish();
+            }
+            else
+            {
+                Snackbar sn = Snackbar.Make(activity_main, "Login Failed", Snackbar.LengthShort);
+                sn.Show();
+            }
         }
     }
 }
---
+++ Ensemble/Ensemble.Android/SignUp.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Tasks;
+using Android.OS;
+using Firebase.Auth;
+using static Android.Views.View;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.Design.Widget;
+
+namespace Ensemble.Droid
+{
+    [Activity(Label = "SignUp", Theme = "@style/AppTheme")]
+    public class SignUp : Activity, IOnClickListener, IOnCompleteListener
+    {
+        Button btnSignup;
+        TextView btnLogin, btnForgetPass;
+        EditText input_email, input_pwd;
+        RelativeLayout activity_sign_up;
+        FirebaseAuth auth;
+
+        public void OnClick(View v)
+        {
+            if (v.Id == Resource.Id.signup_btn_login)
+            {
+                StartActivity(new Intent(this, typeof(MainActivity)));
+                Finish();
+            }
+            //most likely take out
+            else if (v.Id == Resource.Id.signup_btn_forget_password)
+            {
+                StartActivity(new Intent(this, typeof(ForgetPassword)));
+                Finish();
+            }
+            //Edit this to create variable class, add to user database and more
+            else if (v.Id == Resource.Id.signup_btn_register)
+            {
+                SignUpUser(input_email.Text, input_pwd.Text);
+            }
+        }
+
+        private void SignUpUser(string email, string password)
+        {
+            auth.CreateUserWithEmailAndPassword(email, password).AddOnCompleteListener(this, this);
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.SignUp);
+
+            //Init Firebase
+            auth = FirebaseAuth.GetInstance(MainActivity.app);
+
+            //views
+            btnSignup = FindViewById<Button>(Resource.Id.signup_btn_register);
+            btnLogin = FindViewById<Button>(Resource.Id.signup_btn_login);
+            btnForgetPass = FindViewById<Button>(Resource.Id.signup_btn_forget_password); //Take out later
+            input_email = FindViewById<EditText>(Resource.Id.signup_email);
+            input_pwd = FindViewById<EditText>(Resource.Id.signup_password);
+            activity_sign_up = FindViewById<RelativeLayout>(Resource.Id.activity_sign_up);
+
+            btnLogin.SetOnClickListener(this);
+            btnSignup.SetOnClickListener(this);
+            btnForgetPass.SetOnClickListener(this);
+        }
+
+        public void OnComplete(Task task)
+        {
+            if (task.IsSuccessful == true)
+            {
+                Snackbar sn = Snackbar.Make(activity_sign_up, "Register Successfully ", Snackbar.LengthShort);
+                sn.Show();
+            }
+            else
+            {
+                Snackbar sn = Snackbar.Make(activity_sign_up, "Register Failed! ", Snackbar.LengthShort);
+                sn.Show();
+            }
+        }
+    }
+}
