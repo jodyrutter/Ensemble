203c5d99f12a730d8d84baab358ee8afae1723f9
jmilis2000 <jahlil.milis97@gmail.com>
2019-11-07 23:11:58 -0500

    that

--- Ensemble/Ensemble.Android/ChatUsingFirebase.cs
+++ Ensemble/Ensemble.Android/ChatUsingFirebase.cs
@@ -15,16 +15,54 @@ using Firebase.Auth;
 using Firebase.Database;
 using Firebase.Xamarin.Database;
 
+
 namespace Ensemble.Droid
 {
     [Activity(Label = "ChatUsingFirebase")]
-    public class ChatUsingFirebase : Activity
+    public class ChatUsingFirebase : AppCompatActivity, IValueEventListener
     {
+        private FirebaseClient firebase;
+        private List<MessageContent> lstmessages;
+        private ListView lstChat;
+        private EditText edtChat;
+        private FloatingActionButton fab;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
+            SetContentView(Resource.Layout.Messages);
+
+            firebase = new FirebaseClient("https://ensemble-65b0c.firebaseio.com/");
+            FirebaseDatabase.Instance.GetReference("chats").AddValueEventListener(this);
+            fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            edtChat = FindViewById<EditText>(Resource.Id.input);
+            lstChat = FindViewById<ListView>(Resource.Id.list_of_messages);
+
+        }
+
+        public void OnCancelled(DatabaseError error)
+        { 
+        
+        }
+
+        public void OnDataChange(DataSnapshot snapshot)
+        {
+            DisplayChatMessages();
+        }
+
+        private async void DisplayChatMessages()
+        {
+            lstmessages.Clear();
+
+            var items = await firebase.Child("chats")
+                .OnceAsync<MessageContent>();
+            foreach (var item in items)
+                lstmessages.Add(item.Object);
+            ListViewAdapter adapter = new ListViewAdapter(this, lstmessages);
+            //ListViewAdapter adapter = new ListViewAdapter(this, lstmessages);
+            //lstChat.Adapter = adapter;
         }
     }
 }
